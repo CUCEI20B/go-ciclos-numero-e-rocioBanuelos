d0c5d6af7112d854c41ffae1043a65b2ac4305d1
rocioBanuelos <rocio.banuelos4041@alumnos.udg.mx>
2020-10-01 18:37:20 -0500

    Programa que calcula el numero e

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var valor int
+	var resultado float64
+
+	fmt.Scanf("%d\n", &valor)
+
+	if valor >= 0 {
+		resultado = calcularNumeroE(valor)
+		fmt.Print(resultado)
+	} else {
+		fmt.Print("Ingrese un valor mayor o igual a 0")
+	}
+}
+
+func calcularNumeroE(valor int) float64 {
+	var numeroE float64
+
+	numeroE = 0
+
+	for i := 0; i <= valor; i++ {
+		numeroE += float64(1) / float64(calcularFactorial(i))
+	}
+
+	return numeroE
+}
+
+func calcularFactorial(num int) int {
+	var factorial int
+
+	if num == 1 || num == 0 {
+		factorial = 1
+	} else {
+		factorial = 1
+
+		for i := 1; i <= num; i++ {
+			factorial *= i
+		}
+	}
+
+	return factorial
 }
